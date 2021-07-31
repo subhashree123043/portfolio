09dc874d82dbcfcdbfc3b5a1de96652554ea554c
subhashree123043 <85428576+subhashree123043@users.noreply.github.com>
2021-07-31 10:55:27 +0530

    subhashree

--- Portfolio.sql
+++ Portfolio.sql
@@ -1,14 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jul 30, 2021 at 02:57 PM
--- Server version:  8.0.25-0ubuntu0.20.04.1
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Jul 31, 2021 at 07:16 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Portfolio`
+-- Database: `portfolio`
 --
 
 -- --------------------------------------------------------
